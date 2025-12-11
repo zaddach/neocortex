179fb86b07221e14bab99e3369a73948848305d9
Jonas Zaddach <5988756+zaddach@users.noreply.github.com>
2025-12-11 23:24:03 +0100

    Add more mcps

---
+++ postgres/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,7 @@
+CREATE USER openwebui WITH PASSWORD 'openwebui_password';
+CREATE DATABASE openwebui_db OWNER openwebui;
+GRANT ALL PRIVILEGES ON DATABASE openwebui_db TO openwebui;
+
+CREATE USER rag WITH PASSWORD 'rag_password';
+CREATE DATABASE rag_db OWNER rag;
+GRANT ALL PRIVILEGES ON DATABASE rag_db TO rag;
